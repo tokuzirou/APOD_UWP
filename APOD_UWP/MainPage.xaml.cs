1b9fcb362c30f7d160c5eb1c79e790c1088810b3
tokumaM <tokuma.business@outlook.jp>
2021-07-05 01:20:23 +0900

    プロジェクト ファイルを追加します。

---
+++ APOD_UWP/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace APOD_UWP
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //リクエスト先
+        const string EndpointURL = "https://api.nasa.gov/planetary/apod";
+        //Nasaのプロジェクト開始日
+        DateTime launchDate = new DateTime(1995, 6, 15);
+        //ダウンロード数
+        int imageCount;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MonthCalender.MinDate = launchDate;
+            MonthCalender.MaxDate = DateTime.Today;
+        }
+
+        private void LaunchButton_Click(object sender, RoutedEventArgs e)
+        {
+            LimitRangeChackBox.IsChecked = false;
+            MonthCalender.Date = launchDate;
+        }
+    }
+}
